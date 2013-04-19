cdb0e9e4426e7fb1c6f004ad26c6c6ad212db786
Leon Szpilewski <leon.szpilewski@gmail.com>
2013-04-19 20:33:18 +0200

    added flushes

--- report.rb
+++ report.rb
@@ -2,18 +2,30 @@ require './storage'
 
 def ranks_for_app_id(app_id)
   s = Nutte::Storage.new
+	puts "starting metadata query"
+	STDOUT.flush
   app_meta = s.app_metadata_by_app_id(app_id)
+	puts "end metadata query"
+	puts "start rank query"
+	STDOUT.flush
   ranks = s.ranks_by_app_id(app_id)
+	puts "end rank query"
+	STDOUT.flush
   puts "#{app_meta['name']}:"
   puts "ranks: "
   ranks.each {|rank|
     puts rank.to_s
+		STDOUT.flush
   }
 end
 
 def ranks_for_app_name(app_name)
   s = Nutte::Storage.new
+	puts "start app query"
+	STDOUT.flush
   apps = s.find_apps_by_name(app_name)
+	puts "end app query"
+	STDOUT.flush
   apps.each {|app|
     ranks_for_app_id(app['id'])
   }
